744ca926af65f522009ff58cdc0bd2aaa0facaaa
azimov.dev <azimov.dev@gmail.com>
2020-09-23 13:30:26 +0600

    initialize router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { store } from '../store/vuex.js'
+import { store } from '@/store/vuex.js'
+import { router } from '@/router/router.js';
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  store
+  store,
+  router
 }).$mount('#app')
