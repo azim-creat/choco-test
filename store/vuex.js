8f57966db6faca6e18456cfccb905b5d933f84b3
azimov.dev <azimov.dev@gmail.com>
2020-09-23 13:11:05 +0600

    initialize vuex store

---
+++ store/vuex.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+export const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  getters: {
+    count: state => {
+      return state.count;
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
